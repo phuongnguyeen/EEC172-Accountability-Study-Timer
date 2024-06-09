e09592fa261693e6a1f5cacb9572e29ca4ccc8b9
phuongnguyeen <107338371+phuongnguyeen@users.noreply.github.com>
2024-06-08 17:46:49 -0700

    Delete study_timer/Debug directory

--- study_timer/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/Users/phpng/ti/cc3200-sdk/simplelink/ccs/NON_OS_debug/simplelink.a" -l"C:/Users/phpng/ti/cc3200-sdk/driverlib/ccs/Release/driverlib.a" -llibc.a
-
